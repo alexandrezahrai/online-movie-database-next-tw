4b91b8a3a4b009c6b25455e7235f11f8b6213a03
Alex <alexandrezahrai@protonmail.com>
2024-03-25 23:23:52 -0400

    build: creates a skeleton for the movie-details-hero section

---
+++ app/components/ui/skeletons/MovieDetailsHeroSkeleton.tsx
@@ -0,0 +1,7 @@
+export default function MovieDetailsHeroSkeleton() {
+  return (
+    <div className="w-full h-[491px] bg-[#F5F5F5] p-4">
+      <div className="bg-white w-full h-full rounded-[6px] overflow-clip"></div>
+    </div>
+  );
+}
